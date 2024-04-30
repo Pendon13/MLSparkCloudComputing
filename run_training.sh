7171e857aca3be9f023c183027b99fd93009a3a3
Pendon13 <pendon.jon@gmail.com>
2024-04-30 14:28:12 -0400

    commands edited

--- run_training.sh
+++ run_training.sh
@@ -1,4 +1,8 @@
 pip install -r requirements.txt
+sudo -u hdfs hadoop fs -mkdir /user/ec2-user
+sudo -u hdfs hadoop fs -chown ec2-user /user/ec2-user
 hadoop fs -put TrainingDataset.csv
 hadoop fs -put ValidationDataset.csv
 spark-submit modelTraining.py
+
+hadoop fs -copyToLocal /user/ec2-user/job
